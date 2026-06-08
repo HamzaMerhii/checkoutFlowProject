0e051b099adbaa6992a22025bde3613fcccf6efb
Hamza Merhi <hamzamerie60@gmail.com>
2026-06-08 09:52:52 +0300

    thirdCommit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,132 +29,80 @@ let cardNumberValue = [];
 checkoutButton.addEventListener("click", function (event) {
   let isValid = true;
   if (name.value.length < 1) {
-    nameErrorMsg.style.display = "block";
-    name.classList.remove("form-control");
-    name.classList.add("form-control-error");
+    nameValidation(false);
     isValid = false;
   } else {
-    nameErrorMsg.style.display = "none";
-    name.classList.add("form-control");
-    name.classList.remove("form-control-error");
+    nameValidation(true);
   }
   if (mobileNumber.value.length == 0) {
-    mobileErrorMsg.style.display = "block";
-    mobileNumber.classList.remove("form-control");
-    mobileNumber.classList.add("form-control-error");
+    mobileNumberValidation(false, "missing");
     isValid = false;
   } else {
-    mobileErrorMsg.style.display = "none";
-    mobileNumber.classList.add("form-control");
-    mobileNumber.classList.remove("form-control-error");
+    mobileNumberValidation(true, "notMissing");
   }
   if (mobileNumber.value.length !== 8 && mobileNumber.value.length !== 0) {
-    mobileInvalidMsg.style.display = "block";
-    mobileNumber.classList.remove("form-control");
-    mobileNumber.classList.add("form-control-error");
+    mobileNumberValidation(false, "invalid");
     isValid = false;
-  } else {
-    mobileInvalidMsg.style.display = "none";
-    mobileNumber.classList.add("form-control");
-    mobileNumber.classList.remove("form-control-error");
+  } else if (mobileNumber.value.length == 8) {
+    mobileNumberValidation(true, "valid");
   }
   if (email.value.length < 1) {
-    emailErrorMsg.style.display = "block";
-    email.classList.remove("form-control");
-    email.classList.add("form-control-error");
+    emailValidation(false);
     isValid = false;
   } else {
-    emailErrorMsg.style.display = "none";
-    email.classList.add("form-control");
-    email.classList.remove("form-control-error");
+    emailValidation(true);
   }
   if (city.value.length < 1) {
-    cityErrorMsg.style.display = "block";
-    city.classList.remove("form-control");
-    city.classList.add("form-control-error");
+    cityValidation(false);
     isValid = false;
   } else {
-    cityErrorMsg.style.display = "none";
-    city.classList.add("form-control");
-    city.classList.remove("form-control-error");
+    cityValidation(true);
   }
   if (address.value.length < 1) {
-    addressErrorMsg.style.display = "block";
-    address.classList.remove("form-control");
-    address.classList.add("form-control-error");
+    addressValidation(false);
     isValid = false;
   } else {
-    addressErrorMsg.style.display = "none";
-    address.classList.add("form-control");
-    address.classList.remove("form-control-error");
+    addressValidation(true);
   }
   if (onlinePaymentSectionVisible == 1) {
-    console.log(cardNumberValue);
     if (cardName.value.length < 1) {
-      cardNameErrorMsg.style.display = "block";
-      cardName.classList.remove("form-control");
-      cardName.classList.add("form-control-error");
+      cardNameValidation(false);
       isValid = false;
     } else {
-      cardNameErrorMsg.style.display = "none";
-      cardName.classList.add("form-control");
-      cardName.classList.remove("form-control-error");
+      cardNameValidation(true);
     }
     if (cardNumberValue.length !== 16) {
-      cardNumberErrorMsg.style.display = "block";
-      cardNumber.classList.remove("form-control");
-      cardNumber.classList.add("form-control-error");
+      cardNumberValidation(false);
       isValid = false;
     } else {
-      cardNumberErrorMsg.style.display = "none";
-      cardNumber.classList.add("form-control");
-      cardNumber.classList.remove("form-control-error");
+      cardNumberValidation(true);
     }
     if (cvv.value.length !== 3) {
-      cvvErrorMsg.style.display = "block";
-      cvv.classList.remove("form-control");
-      cvv.classList.add("form-control-error");
-    } else {
-      cvvErrorMsg.style.display = "none";
-      cvv.classList.add("form-control");
-      cvv.classList.remove("form-control-error");
-    }
-    if (cardYear.value.length < 4) {
-      cardYearErrorMsg.style.display = "block";
-      cardYear.classList.remove("form-control");
-      cardYear.classList.add("form-control-error");
+      cvvValidation(false);
       isValid = false;
     } else {
-      cardYearErrorMsg.style.display = "none";
-      cardYear.classList.add("form-control");
-      cardYear.classList.remove("form-control-error");
+      cvvValidation(true);
     }
-    if (cardMonth.value.length < 2) {
-      cardMonthErrorMsg.style.display = "block";
-      cardMonth.classList.remove("form-control");
-      cardMonth.classList.add("form-control-error");
+    if (cardYear.value.length !== 4) {
+      cardYearValidation(false, "missing");
+      isValid = false;
+    } else {
+      cardYearValidation(true, "notMissing");
+    }
+    if (cardMonth.value.length !== 2) {
+      cardMonthValidation(false, "missing");
       isValid = false;
     } else if (cardMonth.value < "01" || cardMonth.value > 13) {
-      cardMonthInvalidMsg.style.display = "block";
-      cardMonth.classList.remove("form-control");
-      cardMonth.classList.add("form-control-error");
+      cardMonthValidation(false, "invalid");
       isValid = false;
     } else if (cardYear.value < today.getFullYear()) {
-      cardYearInvalidMsg.style.display = "block";
-      cardMonthInvalidMsg.style.display = "block";
-      cardMonth.classList.remove("form-control");
-      cardMonth.classList.add("form-control-error");
-      cardYear.classList.remove("form-control");
-      cardYear.classList.add("form-control-error");
+      cardYearValidation(false, "invalid");
+      cardMonthValidation(false, "invalid");
       isValid = false;
     } else if (cardYear.value == today.getFullYear()) {
       if (cardMonth.value < today.getMonth()) {
-        cardMonthInvalidMsg.style.display = "block";
-        cardYearInvalidMsg.style.display = "block";
-        cardMonth.classList.remove("form-control");
-        cardMonth.classList.add("form-control-error");
-        cardYear.classList.remove("form-control");
-        cardYear.classList.add("form-control-error");
+        cardYearValidation(true, "invalid");
+        cardMonthValidation(false, "invalid");
         isValid = false;
       }
     }
@@ -172,9 +120,6 @@ checkoutButton.addEventListener("click", function (event) {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#B3D5F3",
       confirmButtonText: "Confirm",
-      customClass: {
-        htmlContainer: "", // Apply custom text class
-      },
     }).then((result) => {
       if (result.isConfirmed)
         Swal.fire({
@@ -193,7 +138,6 @@ checkoutButton.addEventListener("click", function (event) {
             clearInterval(alertTimer);
           },
         }).then((result) => {
-          /* Read more about handling dismissals below */
           if (result.dismiss === Swal.DismissReason.timer)
             Swal.fire({
               title: "Order confirmed!",
@@ -212,127 +156,97 @@ checkoutButton.addEventListener("click", function (event) {
 });
 name.addEventListener("input", function () {
   if (name.value.length >= 1) {
-    nameErrorMsg.style.display = "none";
-    name.classList.add("form-control");
-    name.classList.remove("form-control-error");
+    nameValidation(true);
   }
 });
 mobileNumber.addEventListener("input", function () {
-  if (mobileNumber.value.length == 11) {
-    mobileErrorMsg.style.display = "none";
-    mobileNumber.classList.add("form-control");
-    mobileNumber.classList.remove("form-control-error");
+  if (mobileNumber.value.length == 8) {
+    mobileNumberValidation(true, "notMissing");
+    mobileNumberValidation(true, "valid");
   }
 });
 email.addEventListener("input", function () {
   if (email.value.length >= 1) {
-    emailErrorMsg.style.display = "none";
-    email.classList.add("form-control");
-    email.classList.remove("form-control-error");
+    emailValidation(true);
   }
 });
 city.addEventListener("input", function () {
   if (city.value.length >= 1) {
-    cityErrorMsg.style.display = "none";
-    city.classList.add("form-control");
-    city.classList.remove("form-control-error");
+    cityValidation(true);
   }
 });
 address.addEventListener("input", function () {
   if (address.value.length >= 1) {
-    addressErrorMsg.style.display = "none";
-    address.classList.add("form-control");
-    address.classList.remove("form-control-error");
+    addressValidation(true);
   }
 });
 cardName.addEventListener("input", function () {
   if (cardName.value.length >= 1) {
-    cardNameErrorMsg.style.display = "none";
-    cardName.classList.add("form-control");
-    cardName.classList.remove("form-control-error");
+    cardNameValidation(true);
   }
 });
 cardNumber.addEventListener("keydown", function (event) {
   if (event.key === "Backspace") {
     if (cardNumber.value.charAt(cardNumber.value.length - 1) !== " ") {
       cardNumberValue.pop();
-      console.log(cardNumberValue);
+    }
+    if (cardNumberValue.length === 16) {
+      cardNumberValidation(true);
     }
   }
 });
 cardNumber.addEventListener("keyup", function (event) {
   if (event.key !== "Backspace") {
     if (cardNumberValue.length === 16) {
-      cardNumberErrorMsg.style.display = "none";
-      cardNumber.classList.add("form-control");
-      cardNumber.classList.remove("form-control-error");
+      cardNumberValidation(true);
     }
     if (cardNumber.value.charAt(cardNumber.value.length - 1) !== " ") {
       cardNumberValue.push(cardNumber.value[cardNumber.value.length - 1]);
       if (cardNumberValue.length % 4 == 0) {
         cardNumber.value = cardNumber.value + " ";
       }
+      if (cardNumberValue.length === 16) {
+        cardNumberValidation(true);
+      }
     }
   }
 });
 cvv.addEventListener("input", function () {
   if (cvv.value.length === 3) {
-    cvvErrorMsg.style.display = "none";
-    cvv.classList.add("form-control");
-    cvv.classList.remove("form-control-error");
+    cvvValidation(true);
   }
 });
 cardMonth.addEventListener("input", function () {
-  if (cardMonth.value.length >= 2) {
-    cardMonthErrorMsg.style.display = "none";
-    cardMonth.classList.add("form-control");
-    cardMonth.classList.remove("form-control-error");
+  if (cardMonth.value.length == 2) {
+    cardMonthValidation(true, "notMissing");
   }
   if (cardMonth.value >= "01" || cardMonth.value <= 12) {
-    cardMonthInvalidMsg.style.display = "none";
-    cardMonth.classList.add("form-control");
-    cardMonth.classList.remove("form-control-error");
+    cardMonthValidation(true, "valid");
   }
   if (cardYear.value == today.getFullYear()) {
     if (cardMonth.value >= today.getMonth()) {
-      cardMonthInvalidMsg.style.display = "none";
-      cardYearInvalidMsg.style.display = "none";
-      cardMonth.classList.add("form-control");
-      cardMonth.classList.remove("form-control-error");
-      cardYear.classList.add("form-control");
-      cardYear.classList.remove("form-control-error");
+      cardMonthValidation(true, "valid");
+      cardYearValidation(true, "valid");
     }
   }
   if (cardYear.value > today.getFullYear()) {
-    cardMonthInvalidMsg.style.display = "none";
-    cardYearInvalidMsg.style.display = "none";
-    cardMonth.classList.add("form-control");
-    cardMonth.classList.remove("form-control-error");
-    cardYear.classList.add("form-control");
-    cardYear.classList.remove("form-control-error");
+    cardMonthValidation(true, "valid");
+    cardYearValidation(true, "valid");
   }
 });
 cardYear.addEventListener("input", function () {
-  if (cardYear.value.length >= 2) {
-    cardYearErrorMsg.style.display = "none";
-    cardYear.classList.add("form-control");
-    cardYear.classList.remove("form-control-error");
+  if (cardYear.value.length === 4) {
+    cardYearValidation(true, "notMissing");
   }
   if (cardYear.value >= today.getFullYear()) {
-    cardYearInvalidMsg.style.display = "none";
-    cardYear.classList.add("form-control");
-    cardYear.classList.remove("form-control-error");
+    cardYearValidation(true, "valid");
     if (cardYear.value == today.getFullYear()) {
       if (cardMonth.value >= today.getMonth()) {
-        cardMonthInvalidMsg.style.display = "none";
-        cardMonth.classList.add("form-control");
-        cardMonth.classList.remove("form-control-error");
+        cardMonthValidation(true, "valid");
       }
     }
     if (cardYear.value > today.getFullYear()) {
-      cardMonthInvalidMsg.style.display = "none";
-      cardMonth.classList.add("form-control");
-      cardMonth.classList.remove("form-control-error");
+      cardMonthValidation(true, "valid");
     }
   }
 });
@@ -340,8 +254,152 @@ function updatePaymantSection(value) {
   if (value == 0) {
     onlinePaymentSectionVisible = 0;
     onlinePaymentSection.style.display = "none";
+    cardNameValidation(true);
+    cardNumberValidation(true);
+    cvvValidation(true);
+    cardMonthValidation(true, "notMissing");
+    cardYearValidation(true, "notMissing");
+    cardMonthValidation(true, "valid");
+    cardYearValidation(true, "valid");
   } else if (value == 1) {
     onlinePaymentSectionVisible = 1;
     onlinePaymentSection.style.display = "flex";
+  }
+}
+function nameValidation(status) {
+  if (!status) {
+    nameErrorMsg.style.display = "block";
+    name.classList.remove("form-control");
+    name.classList.add("form-control-error");
+  } else {
+    nameErrorMsg.style.display = "none";
+    name.classList.add("form-control");
+    name.classList.remove("form-control-error");
+  }
+}
+
+function mobileNumberValidation(status, message) {
+  if (!status && message == "missing") {
+    mobileErrorMsg.style.display = "block";
+    mobileNumber.classList.remove("form-control");
+    mobileNumber.classList.add("form-control-error");
+  } else if (status && message == "notMissing") {
+    mobileErrorMsg.style.display = "none";
+    mobileNumber.classList.add("form-control");
+    mobileNumber.classList.remove("form-control-error");
+  } else if (!status && message == "invalid") {
+    mobileInvalidMsg.style.display = "block";
+    mobileNumber.classList.remove("form-control");
+    mobileNumber.classList.add("form-control-error");
+  } else if (status && message == "valid") {
+    mobileInvalidMsg.style.display = "none";
+    mobileNumber.classList.add("form-control");
+    mobileNumber.classList.remove("form-control-error");
+  }
+}
+function emailValidation(status) {
+  if (!status) {
+    emailErrorMsg.style.display = "block";
+    email.classList.remove("form-control");
+    email.classList.add("form-control-error");
+  } else {
+    emailErrorMsg.style.display = "none";
+    email.classList.add("form-control");
+    email.classList.remove("form-control-error");
+  }
+}
+function cityValidation(status) {
+  if (!status) {
+    cityErrorMsg.style.display = "block";
+    city.classList.remove("form-control");
+    city.classList.add("form-control-error");
+  } else {
+    cityErrorMsg.style.display = "none";
+    city.classList.add("form-control");
+    city.classList.remove("form-control-error");
+  }
+}
+
+function addressValidation(status) {
+  if (!status) {
+    addressErrorMsg.style.display = "block";
+    address.classList.remove("form-control");
+    address.classList.add("form-control-error");
+  } else {
+    addressErrorMsg.style.display = "none";
+    address.classList.add("form-control");
+    address.classList.remove("form-control-error");
+  }
+}
+function cardNameValidation(status) {
+  if (!status) {
+    cardNameErrorMsg.style.display = "block";
+    cardName.classList.remove("form-control");
+    cardName.classList.add("form-control-error");
+  } else {
+    cardNameErrorMsg.style.display = "none";
+    cardName.classList.add("form-control");
+    cardName.classList.remove("form-control-error");
+  }
+}
+function cardNumberValidation(status) {
+  if (!status) {
+    cardNumberErrorMsg.style.display = "block";
+    cardNumber.classList.remove("form-control");
+    cardNumber.classList.add("form-control-error");
+  } else {
+    cardNumberErrorMsg.style.display = "none";
+    cardNumber.classList.add("form-control");
+    cardNumber.classList.remove("form-control-error");
+  }
+}
+function cvvValidation(status) {
+  if (!status) {
+    cvvErrorMsg.style.display = "block";
+    cvv.classList.remove("form-control");
+    cvv.classList.add("form-control-error");
+  } else {
+    cvvErrorMsg.style.display = "none";
+    cvv.classList.add("form-control");
+    cvv.classList.remove("form-control-error");
+  }
+}
+function cardYearValidation(status, message) {
+  if (!status && message == "missing") {
+    cardYearErrorMsg.style.display = "block";
+    cardYear.classList.remove("form-control");
+    cardYear.classList.add("form-control-error");
+  } else if (status && message == "notMissing") {
+    cardYearErrorMsg.style.display = "none";
+    cardYear.classList.add("form-control");
+    cardYear.classList.remove("form-control-error");
+  } else if (!status && message == "invalid") {
+    cardYearInvalidMsg.style.display = "block";
+    cardYear.classList.remove("form-control");
+    cardYear.classList.add("form-control-error");
+  } else if (status && message == "valid") {
+    cardYearInvalidMsg.style.display = "none";
+    cardYear.classList.add("form-control");
+    cardYear.classList.remove("form-control-error");
+  }
+}
+
+function cardMonthValidation(status, message) {
+  if (!status && message == "missing") {
+    cardMonthErrorMsg.style.display = "block";
+    cardMonth.classList.remove("form-control");
+    cardMonth.classList.add("form-control-error");
+  } else if (status && message == "notMissing") {
+    cardMonthErrorMsg.style.display = "none";
+    cardMonth.classList.add("form-control");
+    cardMonth.classList.remove("form-control-error");
+  } else if (!status && message == "invalid") {
+    cardMonthInvalidMsg.style.display = "block";
+    cardMonth.classList.remove("form-control");
+    cardMonth.classList.add("form-control-error");
+  } else if (status && message == "valid") {
+    cardMonthInvalidMsg.style.display = "none";
+    cardMonth.classList.add("form-control");
+    cardMonth.classList.remove("form-control-error");
   }
 }
